8bba2207891b84149c224cfb16379963829e7d0d
[stefman123] <[stefangayle@hotmail.co.uk]>
2019-05-23 16:52:44 +0100

    First commit

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/strongly-recommended',
+    '@vue/airbnb',
+  ],
+  rules: {
+    'linebreak-style': 0,
+  },
+};
---
+++ src/Shared/CollapsibleSection.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div class="header">
+      <span
+        v-if="open"
+        @click="open != open">&#x25B2; Collapse </span>
+      <span
+        v-if="!open"
+        @click="open!= open">&#x25B2C; Expand </span>
+    </div>
+    <!-- Toggleable -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CollapasibleSection',
+  data() {
+    return { open: true };
+  },
+
+};
+</script>
+
+<style scoped>
+.header {
+    background-color: #bbb;
+    padding: 3px;
+    cursor: pointer;
+}
+
+</style>
